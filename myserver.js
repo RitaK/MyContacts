0ee58a1ea616f1ede52e67a76d661e430370da7c
Rita Kaufman <rita.kaufman@sap.com>
2017-08-05 15:07:37 +0300

    changes to the NodeJS server

--- myserver.js
+++ myserver.js
@@ -1,5 +1,44 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');  
+
+var mongoose = require('mongoose');
+
+connectToDB();
 
 http.createServer(function(req, res) {
-	res.end(req.url);
+	
+
+	if(req.url)
+	{
+		readFile(req.url, function(err, data){//data - the file's data
+			if(err)
+			{
+				res.end("Path is incorrect.");
+			}
+			else
+			{
+				res.end(data);//response is file's data
+			}
+		});
+	}
 }).listen(8080);
+
+
+function readFile(localPath, cb) {
+	
+   
+    filePath = path.join(__dirname, localPath);
+
+	fs.readFile(filePath, 'utf8', cb);
+}
+
+function connectToDB()
+{
+	mongoose.connect('mongodb://localhost/test');
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function() {
+	  console.log("connection to DB established");
+	});
+}
