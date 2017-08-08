792dc6425c64dade015dfa6de12fc60eb8e4690e
Rita Kaufman <rita.kaufman@sap.com>
2017-08-09 01:12:52 +0300

    contacts appear dynamically on the screen now. when creatin a new contact it refreshes the page and represents all contacts including the new one, sorted by name.

--- main.js
+++ main.js
@@ -3,27 +3,29 @@ var contacts;
 
 getAllContacts();
 
-function createContact(contactName, contactNumder, contactEmail)
-{
-	var contact = {name: contactName, number: contactNumder, email: contactEmail};
-	PostContactData(contact);
-}
-
-
-function addContactRowToGrid(contact)
-{
-
-}
-
-
 function getAllContacts() {
     var result = $.get("http://localhost:8080/getContacts", function(data, status){
     	if(status ==="success")
+    	{
         	contacts = data;
+        	fillTableWithContacts(contacts);
+    	}
+        
         else
         {
         	console.log("The request to get all contacts returned the response status: "+status+ ".");
         }
     });
+}
 
+function fillTableWithContacts(contacts){
+	var contactsObj = JSON.parse(contacts); 
+	for (var i = 0; i < contactsObj.length; i += 1) {
+	$('#contactsGrid').append(
+	  '<div class="contact-row">' +
+	    '<div class="col-md-4 cell">' + contactsObj[i].contact.name + '</div>' +
+	    '<div class="col-md-4 cell">' + contactsObj[i].contact.number + '</div>' +
+	  	'<div class="col-md-4 cell">' + contactsObj[i].contact.email + '</div>' +
+	'</div>');
+	}
 }
